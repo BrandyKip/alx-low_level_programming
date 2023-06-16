3cbded807b2402058a10597651d867935298515d
BrandyKip <kipkuruibrandy@gmail.com>
2023-06-15 20:59:54 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main
+{
+printf("Size of a char : %2d bytes \n", sizeof(char));
+printf("Size of an int : %2d bytes \n", sizeof(int));
+printf("Size of a long int: %2d bytes \n", sizeof(long int));
+printf("size of a long long int : %2d bytes \n", sizeof(long long int));
+printf("Size of a float : %2d bytes \n", sizeof(float));
+
+return(0);
+}
